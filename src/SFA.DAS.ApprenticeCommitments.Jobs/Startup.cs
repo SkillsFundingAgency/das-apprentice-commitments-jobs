39960532456ad4a3825c6ef1c766d014412e6477
Ben Arnold <ben@arnoldfamily.me.uk>
2021-01-07 16:39:19 +0000

    🔧 ESFA standard configuration

--- src/SFA.DAS.ApprenticeCommitments.Jobs/Startup.cs
+++ src/SFA.DAS.ApprenticeCommitments.Jobs/Startup.cs
@@ -1,7 +1,10 @@
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+using SFA.DAS.Configuration.AzureTableStorage;
 using System.IO;
 using System.Reflection;
 
@@ -14,6 +17,7 @@ namespace SFA.DAS.ApprenticeCommitments.Jobs
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.ConfigureLogging();
+            builder.ConfigureConfiguration();
         }
     }
 
@@ -29,6 +33,33 @@ namespace SFA.DAS.ApprenticeCommitments.Jobs
                 var files = Directory.GetFiles(rootDirectory, "nlog.config", SearchOption.AllDirectories)[0];
                 logBuilder.AddNLog(files);
             });
+        }
+
+        internal static void ConfigureConfiguration(this IFunctionsHostBuilder builder)
+        {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+
+            var configuration = serviceProvider.GetService<IConfiguration>();
+
+            var configBuilder = new ConfigurationBuilder()
+                .AddConfiguration(configuration)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables();
+
+            configBuilder.AddJsonFile("local.settings.json", optional: true);
+
+            var preConfig = configBuilder.Build();
+
+            configBuilder.AddAzureTableStorage(options =>
+            {
+                options.ConfigurationKeys = preConfig["ConfigurationNames"].Split(",");
+                options.StorageConnectionString = preConfig["ConfigurationStorage"];
+                options.EnvironmentName = preConfig["EnvironmentName"];
+                options.PreFixConfigurationKeys = false;
+            });
+
+            var config = configBuilder.Build();
+            builder.Services.Replace(ServiceDescriptor.Singleton(typeof(IConfiguration), config));
         }
     }
 }
