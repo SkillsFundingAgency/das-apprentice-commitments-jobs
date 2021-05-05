ef1f5e2d0057ba72377bca15fa68a9f5c339c9c6
Ben Arnold <ben@arnoldfamily.me.uk>
2021-05-05 11:18:01 +0100

    ✔️ Add more Invitation Reminder tests

--- src/SFA.DAS.ApprenticeCommitments.Jobs.UnitTests/TestInvitationReminder.cs
+++ src/SFA.DAS.ApprenticeCommitments.Jobs.UnitTests/TestInvitationReminder.cs
@@ -1,4 +1,5 @@
 ﻿using AutoFixture.NUnit3;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
@@ -11,8 +12,34 @@ namespace SFA.DAS.ApprenticeCommitments.Jobs.UnitTests
     public class TestInvitationReminder
     {
         [Test, AutoMoqData]
-        public async Task Send_reminder_emails([Frozen] Mock<IEcsApi> api, SendInvitationRemindersHandler sut)
+        public async Task Send_reminder_emails_with_default_cutoff([Frozen] Mock<IEcsApi> api, SendInvitationRemindersHandler sut)
         {
+            await sut.Run(null, Mock.Of<ILogger>());
+
+            api.Verify(m => m.SendInvitationReminders(
+                It.Is<SendInvitationRemindersRequest>(
+                    n => n.InvitationCutOffTime.IsCloseTo(DateTime.UtcNow.AddDays(-7), TimeSpan.FromSeconds(1)))));
+        }
+
+        [Test, AutoMoqData]
+        public async Task Send_reminder_emails_with_configured_cutoff([Frozen] Mock<IConfiguration> config,
+            [Frozen] Mock<IEcsApi> api, SendInvitationRemindersHandler sut)
+        {
+            config.Setup(x => x["SendRemindersAfterThisNumberDays"]).Returns("99");
+
+            await sut.Run(null, Mock.Of<ILogger>());
+
+            api.Verify(m => m.SendInvitationReminders(
+                It.Is<SendInvitationRemindersRequest>(
+                    n => n.InvitationCutOffTime.IsCloseTo(DateTime.UtcNow.AddDays(-99), TimeSpan.FromSeconds(1)))));
+        }
+
+        [Test, AutoMoqData]
+        public async Task Send_reminder_emails_with_negative_cutoff_uses_default([Frozen] Mock<IConfiguration> config,
+            [Frozen] Mock<IEcsApi> api, SendInvitationRemindersHandler sut)
+        {
+            config.Setup(x => x["SendRemindersAfterThisNumberDays"]).Returns("-15");
+
             await sut.Run(null, Mock.Of<ILogger>());
 
             api.Verify(m => m.SendInvitationReminders(
