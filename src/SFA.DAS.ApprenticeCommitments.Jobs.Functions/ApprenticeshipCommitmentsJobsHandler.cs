fe4df2d86568648a405274c06a3a17db1dd519aa
Paul Graham <paul.graham@coprime.co.uk>
2021-07-09 15:18:39 +0100

    added logging in message handler

--- src/SFA.DAS.ApprenticeCommitments.Jobs.Functions/ApprenticeshipCommitmentsJobsHandler.cs
+++ src/SFA.DAS.ApprenticeCommitments.Jobs.Functions/ApprenticeshipCommitmentsJobsHandler.cs
@@ -14,26 +14,26 @@ namespace SFA.DAS.ApprenticeCommitments.Jobs.Functions
         , IHandleMessages<ApprenticeshipUpdatedApprovedEvent>
         , IHandleMessages<EmailChangedEvent>
     {
-        private readonly IEcsApi api;
-        private readonly ILogger<ApprenticeshipCommitmentsJobsHandler> logger;
+        private readonly IEcsApi _api;
+        private readonly ILogger<ApprenticeshipCommitmentsJobsHandler> _logger;
 
         public ApprenticeshipCommitmentsJobsHandler(IEcsApi api, ILogger<ApprenticeshipCommitmentsJobsHandler> logger)
         {
-            this.api = api;
-            this.logger = logger;
+            this._api = api;
+            this._logger = logger;
         }
 
         public async Task Handle(ApprenticeshipCreatedEvent message, IMessageHandlerContext context)
         {
             if (message.ContinuationOfId.HasValue)
             {
-                await api.UpdateApprenticeship(message.ToApprenticeshipUpdated());
+                await _api.UpdateApprenticeship(message.ToApprenticeshipUpdated());
             }
             else
             {
-                var res = await api.CreateApprentice(message.ToApprenticeshipCreated());
+                var res = await _api.CreateApprentice(message.ToApprenticeshipCreated());
 
-                logger.LogInformation($"CreateApprentice returned {JsonConvert.SerializeObject(res)}");
+                _logger.LogInformation($"CreateApprentice returned {JsonConvert.SerializeObject(res)}");
 
                 if (res != null)
                 {
@@ -56,10 +56,13 @@ namespace SFA.DAS.ApprenticeCommitments.Jobs.Functions
         }
 
         public Task Handle(ApprenticeshipUpdatedApprovedEvent message, IMessageHandlerContext context)
-            => api.UpdateApprenticeship(message.ToApprenticeshipUpdated());
+            => _api.UpdateApprenticeship(message.ToApprenticeshipUpdated());
 
         public Task Handle(EmailChangedEvent message, IMessageHandlerContext context)
-            => api.UpdateApprenticeEmail(message.ApprenticeId,message.ToEmailUpdate());
+        {
+            _logger.LogInformation($"Received {nameof(EmailChangedEvent)} for apprentice {message.ApprenticeId}");
+            return _api.UpdateApprenticeEmail(message.ApprenticeId, message.ToEmailUpdate());
+        }
 
         public async Task Handle(SendInvitationReply message, IMessageHandlerContext context)
             => await Task.CompletedTask;
