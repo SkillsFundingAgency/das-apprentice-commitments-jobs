7e146024993ad410a30ae53b49777e4fa756c94a
Ben Arnold <ben@arnoldfamily.me.uk>
2021-12-18 11:05:20 +0000

    ✔️ Tests - Handle Account's ApprenticeEmailAddressChanged

---
+++ src/SFA.DAS.ApprenticeCommitments.Jobs.UnitTests/WhenApprenticeEmailAddressHasBeenChanged.cs
@@ -0,0 +1,26 @@
+﻿using AutoFixture.NUnit3;
+using Microsoft.AspNetCore.JsonPatch;
+using Moq;
+using NServiceBus.Testing;
+using NUnit.Framework;
+using SFA.DAS.ApprenticeAccounts.Messages.Events;
+using SFA.DAS.ApprenticeCommitments.Jobs.Api;
+using SFA.DAS.ApprenticeCommitments.Jobs.Functions.EventHandlers.DomainEvents;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeCommitments.Jobs.UnitTests
+{
+    public class WhenApprenticeEmailAddressHasBeenChanged
+    {
+        [Test, AutoMoqData]
+        public async Task Then_notify_apim(
+            [Frozen] Mock<IEcsApi> api,
+            ApprenticeEmailAddressChangedHandler sut,
+            ApprenticeEmailAddressChanged evt)
+        {
+            await sut.Handle(evt, new TestableMessageHandlerContext());
+
+            api.Verify(m => m.UpdateApprentice(evt.ApprenticeId, It.IsAny<JsonPatchDocument<Apprentice>>()));
+        }
+    }
+}
