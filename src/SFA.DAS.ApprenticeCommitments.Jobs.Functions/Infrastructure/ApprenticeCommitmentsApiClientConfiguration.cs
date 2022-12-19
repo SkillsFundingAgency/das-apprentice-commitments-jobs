acdf15e06cd4b786abfef476f78556460678f3c6
chazzysb <cballagan@yahoo.com>
2022-12-19 14:05:36 +0000

    remove old startup

--- src/SFA.DAS.ApprenticeCommitments.Jobs.Functions/Infrastructure/ApprenticeCommitmentsApiClientConfiguration.cs
+++ src/SFA.DAS.ApprenticeCommitments.Jobs.Functions/Infrastructure/ApprenticeCommitmentsApiClientConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using RestEase.HttpClientFactory;
 using SFA.DAS.ApprenticeCommitments.Jobs.Api;
 using SFA.DAS.Http.Configuration;
