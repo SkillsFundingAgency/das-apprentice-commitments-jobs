b51c2a2895227ab57e9a73e1f3ace09f077381c4
chazzysb <cballagan@yahoo.com>
2022-12-23 16:09:05 +0000

    Add wiremock server

---
+++ src/SFA.DAS.ApprenticeCommitments.Jobs.FakeServer/OuterApiBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WireMock.Logging;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace SFA.DAS.ApprenticeCommitments.Jobs.FakeServer;
+
+public class OuterApiBuilder
+{
+    private readonly WireMockServer _server;
+
+    public OuterApiBuilder(int port)
+    {
+        _server = WireMockServer.Start(new WireMockServerSettings
+        {
+            Port = port,
+            UseSSL = true,
+            StartAdminInterface = true,
+            Logger = new WireMockConsoleLogger(),
+        });
+    }
+
+    public static OuterApiBuilder Create(int port)
+    {
+        return new OuterApiBuilder(port);
+    }
+
+    public OuterApi Build()
+    {
+        return new OuterApi(_server);
+    }
+
+    public OuterApiBuilder WithUpdateApproval()
+    {
+        // handle ApprenticeshipUpdatedApprovedEvent
+        _server.Given(
+                Request.Create()
+                    .WithPath("/approvals")
+                    .UsingPut()
+                     )
+            .RespondWith(
+                Response.Create()
+                    .WithStatusCode(HttpStatusCode.OK));
+
+        return this;
+    }
+}
